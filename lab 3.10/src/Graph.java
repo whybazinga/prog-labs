fe6fc85703170f746104544f563f4be0ce8adea8
Alex Muharski <aom9@mail.ru>
2017-12-05 00:49:16 +0300

    fixing lab 10

--- lab 3.10/src/Graph.java
+++ lab 3.10/src/Graph.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class Graph extends Applet {
     public ArrayList<Integer> humidity;
-    int height;
-    int width;
     int count;
+
+    public final int HEIGHT = 600;
+    public final int WIDTH = 900;
 
     public class CoordinateSystem {
         Point center;
@@ -15,9 +16,9 @@ public class Graph extends Applet {
         int csWidth;
 
         CoordinateSystem() {
-            center = new Point(height - PADDING, width - PADDING);
-            csHeight = height - 2 * PADDING;
-            csWidth = width - 2 * PADDING;
+            center = new Point(PADDING, HEIGHT - PADDING);
+            csHeight = HEIGHT - 2 * PADDING;
+            csWidth = WIDTH - 2 * PADDING;
         }
     }
 
@@ -26,30 +27,33 @@ public class Graph extends Applet {
     public Graph() {
         humidity = new ArrayList<>();
         int i = 0;
-        while (this.getParameter("param_0" + i) != null) {
+        /*while (this.getParameter("param_0" + i) != null) {
             humidity.add(Integer.parseInt(getParameter("param_" + i++)));
+        }*/
+        while (i < 4) {
+            humidity.add(i * 20 + (i % 2 == 0 ? 1 : -1) * 10);
+            System.out.println(humidity.get(i));
+            i++;
         }
         count = i;
-        height = this.getSize().height;
-        width = this.getSize().width;
     }
 
     int[][][] makeParts() {
         int[] coordinates = new int[count];
         int partWidth = Math.round((cs.csHeight - 20) / (count - 1));
-        for (int i = 0; i <= count; i++) {
+        for (int i = 0; i < count; i++) {
             coordinates[i] = Math.round((cs.csHeight - 20) * humidity.get(i) / 100);
         }
         int[][][] parts = new int[2][count][4];
-        for (int i = 0; i < count; i++) {
-            parts[0][i][0] = i * partWidth + cs.center.x;
-            parts[0][i][1] = i * partWidth + cs.center.x;
-            parts[0][i][2] = (i + 1) * partWidth + cs.center.x;
-            parts[0][i][3] = (i + 1) * partWidth + cs.center.x;
-            parts[1][i][0] = cs.center.y;
-            parts[1][i][1] = cs.center.y - coordinates[i];
-            parts[1][i][2] = cs.center.y - coordinates[i + 1];
-            parts[1][i][3] = cs.center.y;
+        for (int i = 0; i < count - 1; i++) {
+            parts[0][i][0] = cs.center.x + (i + 1) * partWidth;
+            parts[0][i][1] = cs.center.x + (i + 1) * partWidth;
+            parts[0][i][2] = cs.center.x + i * partWidth;
+            parts[0][i][3] = cs.center.x + i * partWidth;
+            parts[1][i][0] = cs.center.y - coordinates[i + 1];
+            parts[1][i][1] = cs.center.y;
+            parts[1][i][2] = cs.center.y;
+            parts[1][i][3] = cs.center.y - coordinates[i];
         }
         return parts;
     }
@@ -65,7 +69,10 @@ public class Graph extends Applet {
 
     public void paint(Graphics g) {
         drawCoordinateSystem(g);
+        int[][][] parts = makeParts();
+        for (int i = 0; i < count; i++) {
+            g.drawPolygon(parts[0][i], parts[1][i], count);
+        }
     }
-
 
 }
